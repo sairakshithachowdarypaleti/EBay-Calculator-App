2bcdefa9d520cd171283e220e41b888f5c4ed9a3
sairakshithachowdarypaleti <sairakshithachowdarypaleti@gmail.com>
2025-06-03 14:24:11 -0700

    added readME and unit test cases

---
+++ src/test/java/com/ebay/rakshitha/calculator/CalculatorTestApplication.java
@@ -0,0 +1,10 @@
+package com.ebay.rakshitha.calculator;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.ebay.rakshitha.calculator")
+public class CalculatorTestApplication {
+    public static void main(String[] args) {
+
+    }
+}
